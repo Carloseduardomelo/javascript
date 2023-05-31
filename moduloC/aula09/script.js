fb254b566079c5a2633b7fa94543830035d7d418
Carloseduardomelo <127193594+Carloseduardomelo@users.noreply.github.com>
2023-05-31 03:12:47 -0300

    aprendendo DOM

---
+++ moduloC/aula09/script.js
@@ -0,0 +1,17 @@
+/*var p1 = window.document.getElementsByTagName('p')[0]
+document.write(`aqui esta o resuldado ${p1.innerText}`)
+p1.style.color = 'red'*/
+//prgando pela teg html
+
+/*var div = window.document.getElementById('div')
+div.style.backgroundColor = 'black'
+div.innerText = 'tu e muito feo'*/
+//pegando pelo ID
+
+/*var kk = window.document.querySelector('strong')
+kk.style.color ='red'*/
+// para fazer auteração rapidar
+
+/*var h1 = window.document.getElementsByClassName('h')
+h1.style.color = 'red'*/
+//prgando por class
