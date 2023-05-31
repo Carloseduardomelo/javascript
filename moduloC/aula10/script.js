f1ed48981e0b21a265e6205b0d394d2c1b24fd2d
Carloseduardomelo <127193594+Carloseduardomelo@users.noreply.github.com>
2023-05-31 04:16:49 -0300

    .16:48

---
+++ moduloC/aula10/script.js
@@ -0,0 +1,11 @@
+var a = window.document.querySelector('#area')
+
+function click() {
+    a.innerHTML = 'carlos'
+}
+function entrar(){
+    a.innerHTML = 'entrou'
+}
+function saiu(){
+    a.innerHTML = 'saiu'
+}
