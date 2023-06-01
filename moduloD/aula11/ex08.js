88096b7e59be92821dfae839ef85c6a3ff2715bc
Carloseduardomelo <127193594+Carloseduardomelo@users.noreply.github.com>
2023-06-01 17:59:51 -0300

    .

---
+++ moduloD/aula11/ex08.js
@@ -0,0 +1,8 @@
+var vell = 59.9
+console.log(`sua velocidade e de ${vell} KM/H`)
+if (vell > 60){
+    console.log(`voce foi multado por alta vcelocidade`)
+    console.log(`sempre use sinto de segurança`)
+}else{
+    console.log(`boa viagen voce n foi multado`)
+}
---
+++ moduloD/aula11/ex09.js
@@ -0,0 +1,7 @@
+var nal = 'EUA'
+console.log(`Voce esta vivendo em ${nal} agora`)
+if (nal == "Brasil"){
+    console.log('Voce e Brasileiro')
+}else{
+    console.log('Voce e um estrangeiro')
+}
