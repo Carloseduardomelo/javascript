edbd01230f0d8866fba600617a121f938d99e6da
Carloseduardomelo <127193594+Carloseduardomelo@users.noreply.github.com>
2023-06-01 08:29:36 -0300

    estudando dom

--- moduloC/aula10/script.js
+++ moduloC/aula10/script.js
@@ -1,11 +1,17 @@
-var a = window.document.querySelector('#area')
+let a = window.document.querySelector('#area')
+a.addEventListener('click',clicar)
+a.addEventListener('onmouseenter', entrar)
+a.addEventListener('onmouseout', saiu)
 
-function click() {
+
+function clicar(){
     a.innerHTML = 'carlos'
+    a.style.background = 'red'
 }
 function entrar(){
     a.innerHTML = 'entrou'
 }
 function saiu(){
     a.innerHTML = 'saiu'
+    a.style.background = '#68d321'
 }
