0a3a763faa95b1cd424843a7d6f24accfc221f49
Carloseduardomelo <127193594+Carloseduardomelo@users.noreply.github.com>
2023-06-18 15:28:07 -0300

    muita coisa

--- moduloF/aula15/script.js
+++ moduloF/aula15/script.js
@@ -1,2 +1,11 @@
-let num = [1 , 2, 3]
-console.log(num[1])
+/*let nada = [1,2,3,4,5,6]
+console.log(nada[3])*/
+
+
+
+
+let num = ['carlos', 'ana', 'dinheiro', 'moto', 'carro']
+for(let c = 0; c <= num.length -1; c++){
+    console.log(num[c])
+    num.push('carlos')
+}
