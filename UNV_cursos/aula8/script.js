7b9882041c1d9b4caa190c70206b8d3b3b70483a
Carloseduardomelo <127193594+Carloseduardomelo@users.noreply.github.com>
2023-06-24 21:14:23 -0300

    tipos de function

---
+++ UNV_cursos/aula8/script.js
@@ -0,0 +1,17 @@
+// function que não retorna nada
+function fala(){
+    console.log('carlos')
+}
+
+fala()
+
+// function de retorna alguma coisa
+function soma(n1, n2){
+    return console.log(n1 + n2)
+}
+
+soma(45, 45)
+
+
+
+//tipos de function
