f8c23a9ff22b34ef28b416bb1f1d4459a3a8fb4d
Carloseduardomelo <127193594+Carloseduardomelo@users.noreply.github.com>
2023-06-24 22:03:37 -0300

    operadores aritiméticos e de incremento e decremento

---
+++ UNV_cursos/aula010/script.js
@@ -0,0 +1,45 @@
+let valor = 10
+
+// operadores aritiméticos
+// + = adição
+// - = subtração
+// * = multiplicação
+// / = divisão
+// ** = potencia
+
+// adoção
+valor = 10 + 10
+console.log(valor)
+
+// subtração
+valor = 10 - 5
+console.log(valor)
+
+//multiplicação
+valor = 10 * 10
+console.log(valor)
+
+//divisão
+valor = 10 / 2
+console.log(valor)
+
+//potencia
+valor = 5 ** 5
+console.log(valor)
+
+// operadores de incrementação
+// ++
+// --
+let idade = 18
+
+// ++ 
+console.log(++idade)
+//aqui ele adicionar mais 1 a 18
+
+// --
+console.log(--idade)
+// aqui ele vai tira 1 de 18
+
+
+
+// exenplo de operadores aritiméticos e de incrementação
