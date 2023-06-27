9aa657c81edd9d4da0788f8d8d6dc2f619735a3d
Carloseduardomelo <127193594+Carloseduardomelo@users.noreply.github.com>
2023-06-27 09:11:28 -0300

    operadores de igualdade

---
+++ UNV_cursos/aula012/script.js
@@ -0,0 +1,11 @@
+// Operador de igualdade restrida
+console.log(1 === 1) // Aqui ele vai fazer uma verificação para ver si 1 e igual a 1, ele tambem vai fazer a verificação dos tipos, Caso os tipos forem diferentes ele vai retorna false si for igual ele vai retorna true.
+
+// Operador de igualdade solto
+console.log(1 == '1') //Nesse caso ele vai retorna true porque  1 e igual 1, ele não leva enconsiteração os tipos.
+
+// Obs: a melhor for e a primeira
+
+
+
+// Operadores de igualdade
