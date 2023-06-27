dfa69ff95929d7c5721d8328e7ad6c30f17a5f1c
Carloseduardomelo <127193594+Carloseduardomelo@users.noreply.github.com>
2023-06-27 20:45:46 -0300

    operador ternario

---
+++ UNV_cursos/aula013/script.js
@@ -0,0 +1,10 @@
+// operador ternarios
+let valor = 150
+let pobre_rico = valor > 100 ? 'rico' : 'pobre';
+// Aqui fazemos a verificação si valor e maior que 100, si for ele vai receber 'rico' sinão ele vai receber 'pobre'
+console.log(pobre_rico)
+
+
+
+
+// Operadore ternario
