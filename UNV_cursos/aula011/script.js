70362ae569769f88ad8cd47fd9caf0a9fc1b01f3
Carloseduardomelo <127193594+Carloseduardomelo@users.noreply.github.com>
2023-06-27 08:51:51 -0300

    operadores de atribuição

---
+++ UNV_cursos/aula011/script.js
@@ -0,0 +1,24 @@
+//atribuição de valores em uma variavel
+var valor = 10
+console.log(valor)
+
+// atribuição de += 
+valor += 10 // Ele ta atribuino mais dez na variavel valor
+console.log(valor)
+
+// atribuição -=
+valor -= 2 // Ele ta tirando dois da variavel valor
+console.log(valor)
+
+// atribuição *=
+valor *= 3 // A variavel valor  esta sendo mutiplicado por tres 
+console.log(valor)
+
+// atribuição /= 
+valor /= 2 // A variavel avlor esta sendo dividida por dois
+console.log(valor)
+
+// Temos varios operadores de atribuição mias esses acima são os mais utilizados
+
+
+// operadores de atribuição
