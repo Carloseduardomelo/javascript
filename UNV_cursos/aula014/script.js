6ab4d04652337bef50a96fc860d1a87a711057d2
Carloseduardomelo <127193594+Carloseduardomelo@users.noreply.github.com>
2023-06-27 22:25:01 -0300

    .

--- UNV_cursos/aula014/script.js
+++ UNV_cursos/aula014/script.js
@@ -1,7 +1,7 @@
 // Operador logico "E" ele e representado por "&&"
 // obs: "E" so vai voltar TRUE quando os dois lador forem TRUE caso contrario ele vai voutar FALSE
 
-console.log("oprerador logico 'E'")
+console.log("oprerador logico 'AND'")
 console.log( true && true) // vai retorna TRUE
 console.log(true && false) // vai retorna FALSE
 // Obs: si trocarmos o FALSE de lugar não vai fazer diferensa ele vai retorna FALSE
@@ -11,7 +11,7 @@ console.log('-------------------------------------')
 // Operador logico "ou" ele e representado por "||"
 // Obs: Para retorna true so basta um lador ser true
 
-console.log("operador logico 'OU'")
+console.log("operador logico 'OR'")
 console.log(true || false) // Ele vai retorna true
 
 console.log("-------------------------------------")
@@ -19,6 +19,7 @@ console.log("-------------------------------------")
 // operador logico "NÃO" ele e representado pro "!"
 // Obs: ele vai trocar de true para false e de false para true
 
+console.log("operador logico 'NOT'")
 console.log(!true) // Vai retorna FALSE
 console.log(!false) // Vai retorna TRUE 
 
