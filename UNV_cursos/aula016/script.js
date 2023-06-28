c7104fa12480309e1e8a6e8cd96bcfcca550bb66
Carloseduardomelo <127193594+Carloseduardomelo@users.noreply.github.com>
2023-06-28 00:46:21 -0300

    execicio

---
+++ UNV_cursos/aula016/script.js
@@ -0,0 +1,24 @@
+// execicio de trocar valores de variaveis
+
+let a = 'vermelho'
+let b = 'azul'
+
+let c = b + a
+
+console.log(c.slice(0,4))
+console.log(c.slice(4))
+
+console.log('==========OU==========')
+
+let d = 'vermelho'
+let e = 'azul'
+
+let f = d
+
+d = e
+e = f
+
+console.log(d)
+console.log(e)
+
+// execicio completo
