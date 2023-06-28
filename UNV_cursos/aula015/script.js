8f667d382de8e2bf81fc6b8ce52b840b4a68bc44
Carloseduardomelo <127193594+Carloseduardomelo@users.noreply.github.com>
2023-06-27 22:40:21 -0300

    comparação de valores não boleanos

---
+++ UNV_cursos/aula015/script.js
@@ -0,0 +1,19 @@
+/*
+FALSY:
+
+undefined
+null
+0
+false
+
+TRUTHY:
+
+São todos menos os FALSY
+*/
+
+let valor = false || 1
+console.log(valor) // vai retornar 1
+
+// Obs: um pouco complicado de aprender
+
+// Comparação de valores não boleanos
