3ab1ec6197ba3f0ef1175400b76bd000e5add1c5
Carloseduardomelo <127193594+Carloseduardomelo@users.noreply.github.com>
2023-06-27 21:20:18 -0300

    Operadores logicos

---
+++ UNV_cursos/aula014/script.js
@@ -0,0 +1,27 @@
+// Operador logico "E" ele e representado por "&&"
+// obs: "E" so vai voltar TRUE quando os dois lador forem TRUE caso contrario ele vai voutar FALSE
+
+console.log("oprerador logico 'E'")
+console.log( true && true) // vai retorna TRUE
+console.log(true && false) // vai retorna FALSE
+// Obs: si trocarmos o FALSE de lugar não vai fazer diferensa ele vai retorna FALSE
+
+console.log('-------------------------------------')
+
+// Operador logico "ou" ele e representado por "||"
+// Obs: Para retorna true so basta um lador ser true
+
+console.log("operador logico 'OU'")
+console.log(true || false) // Ele vai retorna true
+
+console.log("-------------------------------------")
+
+// operador logico "NÃO" ele e representado pro "!"
+// Obs: ele vai trocar de true para false e de false para true
+
+console.log(!true) // Vai retorna FALSE
+console.log(!false) // Vai retorna TRUE 
+
+
+
+// operadores logicos
