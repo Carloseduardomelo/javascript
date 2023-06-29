5eb106f378b9515228f4c628bb727e6b69c59c49
Carloseduardomelo <127193594+Carloseduardomelo@users.noreply.github.com>
2023-06-29 13:31:42 -0300

    laço de repetição while

---
+++ UNV_cursos/aula020/script.js
@@ -0,0 +1,32 @@
+//? obs: No laço while precisamos criar a variavel fora do laço
+let i = 1
+while(i <= 10){//todo/ aqui fizemos uma verificação si for true ele executa o bloco
+    console.log(`voce e corno pela ${i} vez`)
+    i++ //? obs: sempre temos que colocar o incremento para não ficar infinito o laço
+}
+
+console.log('')
+console.log('numeros impar')
+console.log('')
+
+let imp = 1
+while(imp <= 10){
+    if(imp % 2 != 0){
+        console.log(`o numero ${imp} e impar`)
+    }
+    imp++
+}
+
+console.log('')
+console.log('numeros pares')
+console.log('')
+
+let par = 1
+while(par <= 10){
+    if(par % 2 == 0){
+        console.log(`o numero ${par} e impar`)
+    }
+    par++
+}
+
+//! Loço de repetição WHILE
