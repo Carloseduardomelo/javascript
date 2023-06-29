e9f8c694088193ab9547f21e4726fa840e0d7927
Carloseduardomelo <127193594+Carloseduardomelo@users.noreply.github.com>
2023-06-29 03:18:02 -0300

    switch...case

---
+++ UNV_cursos/aula018/script.js
@@ -0,0 +1,23 @@
+let valor = 10
+switch (valor){ //? aqui passamos a variavel valor
+    case 10: //* aqui fazemos uma verificação para ver se o valor e igual a 10 se for ele vai executar o bloco de codigo.
+        console.log('voce tem dez reais')
+        break
+    case 8:
+        console.log('voce tem oito reais')
+        break
+    case 6:
+        console.log('voce tem seis reais')
+        break
+    case 4:
+        console.log('voce tem quatro reais')
+        break
+    default:
+        console.log('voce tem 0 reais')            
+}
+
+//todo obs: Sempre use um break depois de cada case.
+//todo obs: o default ele e usado para caso menhama das verificação for true ele e uma "else" da vid.
+
+
+//! SWITCH..CASE
