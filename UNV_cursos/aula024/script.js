d67c6b491ae1352569d87f336089a1e2ffdb6061
Carloseduardomelo <127193594+Carloseduardomelo@users.noreply.github.com>
2023-06-29 15:39:29 -0300

    execicio

---
+++ UNV_cursos/aula024/script.js
@@ -0,0 +1,10 @@
+//! execicio para saber o maior valor 
+
+let maior = max(39,1)
+console.log(maior)
+
+function max(num1,mun2){
+    return num1 > mun2 ? num1: mun2;
+}
+
+//! muito facil mais muito dicicil 
