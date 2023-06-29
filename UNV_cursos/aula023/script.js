46483b97a2b97fb8eaa35211f299736dddda869b
Carloseduardomelo <127193594+Carloseduardomelo@users.noreply.github.com>
2023-06-29 14:51:16 -0300

    FOR...OF

---
+++ UNV_cursos/aula023/script.js
@@ -0,0 +1,12 @@
+//todo/ O for of e a mesma coisa do for in so que não precisamos passar o array e passar o indici ele faz direto
+
+let cores = ['verde', 'amarelho', 'vermelho']
+for(let cor of cores){
+    console.log(cor)
+}
+
+
+//* com isso ele vai mostrar todos os valores dentro da array
+
+
+//! FOR...OF
