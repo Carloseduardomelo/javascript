13a80212daf498b57a365bdd66a849b707fd9a8d
Carloseduardomelo <127193594+Carloseduardomelo@users.noreply.github.com>
2023-06-29 03:48:55 -0300

    Laço de repetição FOR

---
+++ UNV_cursos/aula019/script.js
@@ -0,0 +1,27 @@
+for(let i = 1; i <= 10; i++){
+    //? criamos uma variavel
+    //? fazemos uma verificação
+    //? depois fazemos o incremento
+    console.log(`voce e corno pela ${i}°`)
+}
+console.log('')
+console.log('ou comtrario')
+console.log('')
+
+for(let c = 10; c >= 1; c--){
+    console.log(`voce e corno pela ${c}°`)
+}
+
+console.log('')
+console.log('numeros impas')
+console.log('')
+
+for(let b = 1; b <= 10; b++){
+    if(b % 2 != 0){
+        console.log(`o numero ${b} e empar`)
+    }
+}
+
+
+
+//! Laço de repetição FOR
