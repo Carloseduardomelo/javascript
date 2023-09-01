0d3ba12d5c1b22d0e01ed5e345f8ad2e4b082882
Carloseduardomelo <127193594+Carloseduardomelo@users.noreply.github.com>
2023-09-01 15:19:25 -0300

    exe feito

---
+++ inpulso-javacript-dio/modulo03/ex04/assets/scripts/script.js
@@ -0,0 +1,21 @@
+const button = document.getElementById('button')
+const h1 = document.getElementById('h1')
+const main =  document.getElementById('main')
+const footer = document.getElementById('fod')
+
+let offOn = true
+
+
+button.addEventListener('click' , () => {
+    if(offOn === true ){
+        main.classList.add('dark')
+        footer.classList.add('darkf')
+        h1.innerHTML = 'dark model on'
+        offOn = false
+    }else{
+        main.classList.remove('dark')
+        footer.classList.remove('darkf')
+        h1.innerHTML = 'light mode on'
+        offOn = true
+    }
+})
